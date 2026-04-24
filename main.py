ec0adf90d73cf1f761b6176ff4e7be999e89df85
AlcidesNeto32 <Alcidesnda@gmail.com>
2026-04-24 01:30:47 -0300

    feat: foram feitas correções ortográficas e adicionado um tratamento de erro caso o input seja diferente das demonstradas no menu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import entity.Cep
+from entity import Cep
+cep = Cep
 
-cep = entity.Cep
 apresentacao = """-----------------------------------------
 Bem vindo ao check CEP!
 -----------------------------------------
@@ -14,32 +14,31 @@ print(apresentacao)
 ceps_anteriores = []
 while(True):
     print(menu)
-    opcao = int(input("SELECIONE UMA DAS OPÇÕES ACIMA: "))
-    print()
-    if opcao == 1:
-        requisicao = input('Digite o cep: ')
-        ceps_anteriores.append(cep.Cep(requisicao).search_cep(requisicao))
-        print(cep.Cep(requisicao).search_cep(requisicao))
-        if not input('Deseja proucurar usar outro serviço? S/N ').upper() == 'S':
+    try:
+        opcao = int(input("SELECIONE UMA DAS OPÇÕES ACIMA: "))
+        print()
+        if opcao == 1:
+            requisicao = input('Digite o cep: ')
+            ceps_anteriores.append(cep.Cep(requisicao).search_cep(requisicao))
+            print(cep.Cep(requisicao).search_cep(requisicao),"\n")
+            if not input('Deseja procurar usar outro serviço? S/N ').upper() == 'S':
+                print("Obrigado por usar nosso serviço, até mais!")
+                break
+        elif opcao == 2:
+            if len(ceps_anteriores) == 0:
+                print("Nenhum CEP na lista \n")
+                print()
+
+            for a,item in enumerate(ceps_anteriores,1):
+                print(f'{a}°\n{item} \n')
+        elif opcao == 3:
             print("Obrigado por usar nosso serviço, até mais!")
             break
-        print()
-    elif opcao == 2:
-        cont = 1
-        if len(ceps_anteriores) == 0:
-            print("Nenhum CEP na lista")
-            print()
-            continue
-
-        for item in ceps_anteriores:
-            print(f'{cont}° {item}')
-        print()
-    elif opcao == 3:
-        print("Obrigado por usar nosso serviço, até mais!")
-        break
-    else:
+        else:
+            print("Essa opção não existe!")
+    except ValueError:
         print("Essa opção não existe!")
-        continue
+
 
 
 
