f2029f7a1b517ba116ac58a4cb6a92f8079e7d55
AlcidesNeto32 <Alcidesnda@gmail.com>
2026-04-29 00:22:34 -0300

    refactor: o historico de ceps foi removido e a instância da classe Cep foi refeita.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from entity import Cep
-cep = Cep
+from entity.Cep import Cep
+cep = Cep()
 
 apresentacao = """-----------------------------------------
 Bem vindo ao check CEP!
@@ -11,7 +11,6 @@ menu = """SELECIONE O SERVICO:
 3-SAIR
 """
 print(apresentacao)
-ceps_anteriores = []
 while(True):
     print(menu)
     try:
@@ -19,18 +18,12 @@ while(True):
         print()
         if opcao == 1:
             requisicao = input('Digite o cep: ')
-            ceps_anteriores.append(cep.Cep(requisicao).search_cep(requisicao))
-            print(cep.Cep(requisicao).search_cep(requisicao),"\n")
+            print(cep.search_cep(requisicao),"\n")
             if not input('Deseja procurar usar outro serviço? S/N ').upper() == 'S':
                 print("Obrigado por usar nosso serviço, até mais!")
                 break
         elif opcao == 2:
-            if len(ceps_anteriores) == 0:
-                print("Nenhum CEP na lista \n")
-                print()
-
-            for a,item in enumerate(ceps_anteriores,1):
-                print(f'{a}°\n{item} \n')
+            cep.mostrar_ceps_anteriores()
         elif opcao == 3:
             print("Obrigado por usar nosso serviço, até mais!")
             break
