84e9d1236bf86019cac8dd9406cfbaff50f86724
AlcidesNeto32 <Alcidesnda@gmail.com>
2026-04-29 00:17:22 -0300

    refactor: removido parâmetro do construtor, historico de ceps removido do arquivo principal para outro e foram adicionado o método para mostrar os CEPs anterirores

--- entity/Cep.py
+++ entity/Cep.py
@@ -4,18 +4,21 @@ import re
 
 class Cep:
 
-    def __init__(self,cep):
-        self.cep = cep
+    def __init__(self):
+        pass
+
+    global historico_cep
+    historico_cep = []
 
     @functools.cache
     def search_cep(self,cep):
         padrao_cep = re.compile(r'(\d){5}|-(\d){3}')
 
         if not padrao_cep.match(cep):
-            return f"O CEP: {self.cep} está inválido!"
-
+            return f"O CEP: {cep} está inválido!"
+        
         try:
-             response = rq.get(f'https://viacep.com.br/ws/{self.cep}/json/')
+             response = rq.get(f'https://viacep.com.br/ws/{cep}/json/')
              response.raise_for_status()
 
         except rq.exceptions.HTTPError as e:
@@ -44,5 +47,15 @@ BAIRRO: {bairro}
 LOGRADOURO: {logradouro}
 UF: {uf}
 REGIÃO: {regiao}"""
+        historico_cep.append(formatacao_dados)
         return formatacao_dados
-# tinker,dar espaço no historio, e ver o loop,cache,erros ortográficos
+    
+
+    def mostrar_ceps_anteriores(self):
+        if len(historico_cep) == 0:
+            return 'Nenhum cep no histórico!'
+
+        for pos,item in enumerate(historico_cep,1):
+            print (f'{pos}°\n{item}\n')
+
+
